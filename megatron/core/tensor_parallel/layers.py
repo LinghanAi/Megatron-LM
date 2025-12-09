371a1d9f980b24a1c84d25fb99770e8e4b4780d2
艾凌涵 <24373115@buaa.edu.cn>
2025-12-09 13:30:50 +0800

    minus_exp = None

--- megatron/core/tensor_parallel/layers.py
+++ megatron/core/tensor_parallel/layers.py
@@ -838,12 +838,11 @@ class CustomLinearWithGradAccumulationAndAsyncCommunication(torch.autograd.Funct
         from fake_quant_ops.quant.ops.bf16_operators import bf16_matmul
         
         if custom_quant_type == 'mxfp4':
-            print("DEBUG: minus_exp set to 1")
             output = mxfp_matmul(
                 total_input, weight.t(),
                 elem_format='fp4_e2m1',
                 block_size=32,
-                minus_exp=1,
+                minus_exp=None,
             )
 
         elif custom_quant_type == 'mxfp8':
